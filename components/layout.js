7a139ff482021f695647c250d706c92f90d5fae4
Axel <axosar@gmail.com>
2021-02-01 13:40:07 -0600

    Entrada diseño

---
+++ components/layout.js
@@ -0,0 +1,22 @@
+import Head from "next/head";
+
+const Layout = ({children}) => (
+  <>
+    <Head>
+      <meta charSet="utf-8" />
+      <title>MAPEI | IMCYC Aplicaciones para la construcción</title>
+      <link rel="icon" type="image/png" href="favicon.png" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;700&display=swap" rel="stylesheet" />
+      <link rel="stylesheet" href="https://cdn.linearicons.com/free/1.0.0/icon-font.min.css"></link>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"></link>
+    </Head>
+
+    {children}
+  
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+  </>
+);
+
+export default Layout;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,29 @@
+import Layout from '../components/layout';
 import Link from 'next/link'
+
 export default function Home() {
   return (
-    <div>
-      Hello World.{' '}
-      <Link href="/about" as={process.env.BACKEND_URL + '/about'}>
-        <a>About</a>
-      </Link>
-    </div>
+    <Layout>
+      <div className="intro">
+        <div className="login">
+          <h2>PLATAFORMA EDUCATIVA LATINOAMÉRICA</h2>
+          <img src="images/intro-mapei.jpg" alt="" title="" />
+          <h3>WEBINARS EN CONCRETO</h3>
+          <form>
+            <p>INGRESAR A SU CUENTA</p>
+            <div>
+              <input type="email" class="form-control" placeholder="Nombre de usuario" />
+            </div>
+            <div>
+              <input type="password" class="form-control" placeholder="Contraseña" />
+            </div>
+            <Link href="/about" as={process.env.BACKEND_URL + '/about'}>
+              <a class="btn btn-primary float-right">ENTRAR</a>
+            </Link>
+          </form>
+          
+        </div>
+      </div>
+    </Layout>
   )
 }
